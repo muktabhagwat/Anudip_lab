fbbb1d64877e6f202c7b7ea7b76f94bc8ad13dab
Muktanali Ghangale <150799174+muktabhagwat@users.noreply.github.com>
2023-11-16 14:50:55 +0530

    Update RateOfInterest.java

--- RateOfInterest.java
+++ RateOfInterest.java
@@ -10,7 +10,7 @@ public class RateOfInterest {
 		System.out.println("\nEnter your choice for select gender\n1: Male\n2:Female");
 		choice=sc.nextInt();
 		System.out.println("Enter the Age of person");
-		age=sc.nextInt();
+		age=sc.nextInt();//accept the input number from user for age
 		if(choice==1)
 		{ 
 			if(age>=60)
