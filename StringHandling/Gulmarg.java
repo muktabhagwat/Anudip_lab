9b8546929b727123d3c9f18d5d04258bad568a3c
Muktanali Ghangale <150799174+muktanjali01@users.noreply.github.com>
2024-05-14 16:56:57 +0530

    Add files via upload

---
+++ StringHandling/Gulmarg.java
@@ -0,0 +1,19 @@
+package StringHandling;
+
+
+class Gulmarg extends HillStation 
+{
+
+	@Override
+	void location() 
+	{
+		System.out.println("In District Barmula, Alambal Road, Gulmarg");
+	}
+
+	@Override
+	void famousfor() 
+	{
+		System.out.println("Gulmarg Gondola");
+	}
+
+}
---
+++ StringHandling/Mussorie.java
@@ -0,0 +1,18 @@
+package StringHandling;
+
+class Mussoorie extends HillStation
+{
+
+	@Override
+	void location() 
+	{
+		System.out.println("In Massoorie , Uttarakhand");
+	}
+
+	@Override
+	void famousfor() 
+	{
+		System.out.println("Kempty Falls");
+	}
+	
+}
